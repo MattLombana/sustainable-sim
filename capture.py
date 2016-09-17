2e1cbd80f460a6e10c2d8a6a71e4315e79bd3a4d
Matt Lombana <mjlombana@icloud.com>
2016-09-17 17:21:54 -0400

    update camera implementation

--- capture.py
+++ capture.py
@@ -10,13 +10,32 @@ username = ss['server']['username']
 ip_address = ss['server']['ip_address']
 path = "{}/{}".format(ss['server']['photo_path'], filename)
 
-pygame.camera.init()
-camera_list = pygame.camera.list_cameras()
-camera_name = camera_list[0]
-cam = pygame.camera.Camera(camera_name, (2048, 1620))
-cam.start()
-img = cam.get_image()
-pygame.image.save(img, filename)
-cam.stop()
 
-os.system("scp {} {}@{}:{}".format(filename, username, ip_address, path))
+
+
+def start_camera():
+    """Creates, starts, and returns a camera object"""
+    pygame.camera.init()
+    camera_list = pygame.camera.list_cameras()
+    camera_name = camera_list[0]
+    cam = pygame.camera.Camera(camera_name, (2048, 1620))
+    cam.start()
+    return cam
+
+def take_photo(camera):
+    """Takes a photo, and uploads it to a remote server
+    Parameters:
+        camera: a camera object
+    """
+    img = camera.get_image()
+    pygame.image.save(img, filename)
+    os.system("scp {} {}@{}:{}".format(filename, username, ip_address, path))
+    os.remove(filename)
+
+
+def stop_camera(camera):
+    """ Stops the camera.
+    Parameters:
+        camera: a camera object
+    """
+    camera.stop()
--- gui.py
+++ gui.py
@@ -1,18 +1,20 @@
-import tkinter
 from tkinter import Label
 from PIL import ImageTk
+import capture
 import Game as g
+import tkinter
 
 class MyFirstGUI:
     def __init__(self, master):
         self.master = master
         master.title("A simple GUI")
 
+        self.camera = capture.start_camera()
         game = g.Game()
         self.curr = game.start
         self.choices = self.curr.getOptions()
 
-        self.B = tkinter.Button(master, text=self.choices[0], command=lambda: self.update(self.choices[0]))
+        self.B = tkinter.Button(master, text=self.choices[0], command=lambda: self.update(self.choices[0], self.camera))
         self.B.pack()
 
         self.img = ImageTk.PhotoImage(self.curr.getImage())
@@ -20,7 +22,7 @@ class MyFirstGUI:
         self.panel.image = self.img
         self.panel.pack()
 
-    def update(self, key):
+    def update(self, key, camera):
         """update the image and the buttons to the next node, waiting
         for the camera and happiness judgement to happen if necessary.
         """
@@ -35,6 +37,10 @@ class MyFirstGUI:
         print(key)
         # nextnode = self.curr.getNext(key)
         # choices = list(self.curr.nexts.keys())
+        # TODO: if cannot progress:
+        #   capture.take_photo(camera)
+        if len(choices) == 0:
+            capture.stop_camera(camera)
 
         # # self.curr = nextnode
         # print("something here")
