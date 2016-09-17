5d1ceea930f8a23b77a449d35100ab6a130f652e
Matt Lombana <mjlombana@icloud.com>
2016-09-17 16:31:26 -0400

    organize media

--- Game.py
+++ Game.py
@@ -1,16 +1,16 @@
 from GameNode import GameNode as gn
 
 class Game:
-    start = gn("first thing", "a.gif")
-    second = gn("second thing", "b.gif", start, "tob")
-    third = gn("also off the first thing", "c.gif", start, "toc")
-    end = gn("rome", "rome.gif")
+    start = gn("first thing", "./assets/images/a.gif")
+    second = gn("second thing", "./assets/images/b.gif", start, "tob")
+    third = gn("also off the first thing", "./assets/images/c.gif", start, "toc")
+    end = gn("rome", "./assets/images/rome.gif")
 
-    fourth = gn("another", "a.gif")
-    fifth = gn("asdf", "a.gif")
-    sixth = gn("23f3f", "a.gif")
-    seventh = gn("anasdfsdfother", "a.gif")
-    eighth = gn("gwg23g", "a.gif")
+    fourth = gn("another", "./assets/images/a.gif")
+    fifth = gn("asdf", "./assets/images/a.gif")
+    sixth = gn("23f3f", "./assets/images/a.gif")
+    seventh = gn("anasdfsdfother", "./assets/images/a.gif")
+    eighth = gn("gwg23g", "./assets/images/a.gif")
 
     fourth.link("4-5", fifth)
     fifth.link("5-6", sixth)
