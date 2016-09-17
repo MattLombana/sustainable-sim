0452bb8300e83c4e991d3db6697c82b459ae795d
Jane Du <zd53@cornell.edu>
2016-09-17 16:41:09 -0400

    Add stop for camera flag

--- GameNode.py
+++ GameNode.py
@@ -3,13 +3,14 @@ from PIL import Image
 class GameNode:
    'Common base class for all employees'
 
-   def __init__(self, text, image, parent=None, key=None):
+   def __init__(self, text, image, parent=None, key=None, canprogress=True):
       """Creates a new scene w/ image, text, and (optionally) next node"""
       self.image = Image.open(image)
-      self.text = "Alex: The text that will be displayed, M-kun."
+      self.text = "Alex: The text that will be displayed, P-kun."
       self.nexts = {}
       if (parent):
          parent.link(key, self)
+      self.canprogress = canprogress
 
    def getOptions(self):
       """Gets all possible choices that the player can choose"""
@@ -23,11 +24,15 @@ class GameNode:
 
    def getNext(self, key):
       """Returns next scene, given that the player chose "key" """
-      if key in self.nexts:
-         return self.nexts[key]
+      if self.canprogress:
+         if key in self.nexts:
+            return self.nexts[key]
+         else:
+            print ("Can't find the next page!")
+            return GameNode("Sorry, how did you end up here?", "lost.gif")
       else:
-         print ("Can't find the next page!")
-         return GameNode("Sorry, how did you end up here?", "lost.gif")
+         self.text = self.text + "\nOh no, P-kun! Are you really sure you mean that? You're not really smiling."
+         return self
 
    def link(self, key, newnode):
       """Links self to point to newnode, if you choose option "key" """
