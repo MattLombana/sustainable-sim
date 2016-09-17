6665b082b77c24f50c0bf1048505861b83ca85ce
Jane Du <zd53@cornell.edu>
2016-09-17 06:51:47 -0400

    Create skeleton for expression test

---
+++ expressiontest.py
@@ -0,0 +1,17 @@
+threshold = 0.90 # arbitrary threshold; user must be happier than this
+
+
+def sendphoto():
+   """gets the photo url, makes request to Emotion API to get json """
+
+
+def parseEmotions():
+    """return a float representing how happy the user is in the photo"""
+    # parse the json here
+
+def isHappyEnough():
+    """return a boolean of whether or not the user is happier than threshold"""
+
+
+if __name__ == '__main__':
+    isHappyEnough()
