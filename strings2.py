2e3b77cb8be99996ceda377382c6a453645d2259
kartick sv <kartick sv>
2016-09-18 03:51:40 -0400

    'strings2'

---
+++ strings2.py
@@ -0,0 +1,53 @@
+#Break ankle scene
+Al="Ah! Ouch!"
+You="Are you alright?"
+Al_1="I think I twisted my ankle. I really dont think I can go on."# al is on the ground 
+response_1="Common on, get up! We can't keep Eve waiting"
+response_2=" Ok let me help you up and we can take a look at it!"
+# Flash Light scene
+#while user takes a look at it
+Al_2="How bad is it? It's not too bad right?"
+Eve="Stop being a wuss! It's probably just a minor bruise."
+You_1="Well, I could tell if I had a flash light. I can barely see a thing!"
+
+#tractor beam silence even in the forest
+"......"
+Unid="Alex of Lafayette! You are the chosen one! You have been identified as the most environmentally concerned!"# both eve and alex are on the frame
+Eve_1="Hey look a UFO! Let me take a picture! User smile"
+response_5="What?"# takes picture requries surprise
+#If not smile
+Eve_2="User!!I said smile!"#else go into next mod
+#alein interaction
+Eve_3="And Mr. unidentified voice what do you mean by that he is the most environmentally concerned? Did you know that I recycled 2 pop cans last week?"
+
+You_2="Talking about recycling, Did you know the average person has the opportunity to recycle more than 25,000 cans in a lifetime?"
+#Sounds of beams on alex he is getting sucked in
+Al_6="Oh God!What's happening?"
+Eve_4="Common user let's go!"
+You_7="But what about Alex? We can't abandon him"
+Eve_5="We dont have time! We gotta get going!"
+response_3="Sorry Alex!I promise I'll come back!" #leads to node where you run into an alein
+response_4="No! I am not gonna leave him! We were all in this together!!" 
+
+#next scene
+#flashlight goes off 
+
+
+#next screen blacks out , thud sounds 
+MUM="Honey! Wake up , you were talking in your sleep again!"
+#wakes up in bed
+MUM_1= "You need start getting ready! Your bus will be here any time now!"
+You_5="For what?"
+MUM_2="It's the last day of school"
+You_6="Mom Did you know that..."
+#at school
+promt="?:Hey you!"
+"........"
+
+
+
+
+
+
+
+
