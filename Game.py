80cbfc9011557d4a37cd47dcb3b04b5799be2303
Jane Du <zd53@cornell.edu>
2016-09-18 07:13:38 -0400

    starting constr

--- Game.py
+++ Game.py
@@ -2,18 +2,25 @@ from GameNode import GameNode as gn
 
 
 class Game:
+    # end_node = gn("Thanks for playing our game!", "./assets/images/rome.gif")
+    # bad_ending_node = gn("Unfortunately, that was a bad choice. Better luck next time!", "./assets/images/pme.jpg", "End Game", end_node)
+
+    # after_look_up = gn('random prompt', "b.gif")
+
+    # look_up = gn("{}".format(), "{}".format())
+
+    # desk_node = gn("{}".format(), "./assets/images/desk.jpg", button_1="{}".format(), look_up)
+
+    # start_node = gn("This should be the first panel.", "./assets/images/Alex in High School.jpg", "Lets Get Going!", desk_node)
+
+
     end_node = gn("Thanks for playing our game!", "./assets/images/rome.gif")
     bad_ending_node = gn("Unfortunately, that was a bad choice. Better luck next time!", "./assets/images/pme.jpg", "End Game", end_node)
 
-    after_look_up = gn('random prompt', "b.gif")
 
-    look_up = gn("{}".format(), "{}".format())
+# def __init__(self, prompt="", image="assets/images/lost.gif", button_1="", child_1=None, button_2="", child_2=None, can_progress=True)
 
-    desk_node = gn("{}".format(), "./assets/images/desk.jpg", button_1="{}".format(), look_up)
-
-    start_node = gn("This should be the first panel.", "./assets/images/Alex in High School.jpg", "Lets Get Going!", desk_node)
-
-
+    instruction = gn("You are being asked to take a photo right now", "./assets/images/b.gif", start, "Go from statrt to second.", False)
 
     # second = gn("You are being asked to take a photo right now", "./assets/images/b.gif", start, "Go from statrt to second.", False)
     # second.link("But all roads lead to", end)
@@ -31,8 +38,6 @@ class Game:
     # fifth.link("5-6", sixth)
     # sixth.link("6-7", seventh)
     # seventh.link("7-8", eighth)
-
-
 
 
     # eighth.link("But all roads lead to", end)
