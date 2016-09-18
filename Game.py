e9da19ff7eefe6aa7f28f0840de2fc3f741ab75b
Jane Du <zd53@cornell.edu>
2016-09-18 00:50:39 -0400

    Add disclaimer

--- Game.py
+++ Game.py
@@ -2,7 +2,10 @@ from GameNode import GameNode as gn
 
 class Game:
     end = gn("Thanks for playing our game!", "./assets/images/rome.gif")
-    start = gn("This should be the first panel.", "./assets/images/Alex in High School.jpg")
+    start = gn("Hey there! Thanks for checking out our game for" +
+        "BigRed//Hacks. It was made with love and sustainability in mind.\n" + 
+        "Please keep in mind: we will be using your laptop camera to take photos of you" + 
+        " during the game. We hope you have fun exploring!", "./assets/images/High School.jpg")
 
     second = gn("You are being asked to take a photo right now", "./assets/images/b.gif", start, "Go from statrt to second.", False)
     second.link( "But all roads lead to", end)
