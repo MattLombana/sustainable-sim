7f37a026a19d73863efab53b236c220a6b3daf97
kartick sv <kartick sv>
2016-09-18 06:12:49 -0400

    Combine finally

--- strings.py
+++ strings.py
@@ -66,11 +66,49 @@ adis =  "Alex gave up and went home, and you and Eve barely explored the forest 
 saycheese = "Hey, let's take a picture. Say water conservation!"
 
 
-#images
-desk = ""
-al_class = ""
-al_class_2 = ""
-eve_barges_in = ""
-ev_class = ""
-eve_fence = ""
-scary_fence = ""
+#Break ankle scene
+Break_ankle_prompt="Alex:Ah! Ouch!\nYou:Are you alright?\nAlex:I think I twisted my ankle. I really dont think I can go on."# al is on the ground 
+response_1="Common on, get up! We can't keep Eve waiting"
+response_2=" Ok let me help you up and we can take a look at it!"
+# Flash Light scene
+#while user takes a look at it
+Flashlight_prompt="Alex:How bad is it? It's not too bad right?\nEve:Stop being a wuss! It's probably just a minor bruise.\nYou:Well, I could tell if I had a flash light. I can barely see a thing!"
+
+#tractor beam silence even in the forest
+
+Tracer_beam_prompt="Unidentified voice:Alex of Lafayette! You are the chosen one! You have been identified as the most environmentally concerned!\nEve:Hey look a UFO! Let me take a picture! smile!"
+response_5="What?"# takes picture requries surprise
+#If not smile
+If_not_smile_prompt="Eve:User!!I said smile!"#else go into next mod
+#alein interaction
+Alein_interaction_prompt="Eve:And Mr. unidentified voice what do you mean by that he is the most environmentally concerned? Did you know that I recycled 2 pop cans last week?\nYou:Talking about recycling, Did you know the average person has the opportunity to recycle more than 25,000 cans in a lifetime?""
+
+
+
+
+#Sounds of beams on alex he is getting sucked in
+sucked_in_prompt="Alex:Oh God!What's happening?\nEve:Come on user let's go!\nYou:But what about Alex? We can't abandon him\nEve:We dont have time! We gotta get going!"
+response_3="Sorry Alex!I promise I'll come back!" #leads to node where you run into an alein
+response_4="No! I am not gonna leave him! We were all in this together!!" 
+
+#next scene
+#flashlight goes off 
+
+
+#next screen blacks out , thud sounds 
+MUM_prompt="Mom:Honey! Wake up , you were talking in your sleep again!"
+#wakes up in bed
+wake_bed_prompt= "Mom:You need start getting ready! Your bus will be here any time now!\nYou:For what?\nMom:It's the last day of school"
+response_7="You:Mom Did you know that..."
+#at school
+at_school_promt="?:Hey you!"
+"........"
+
+
+#hell node 
+hell_prompt="Unfortunately that was a bad choice. Better luck next time."
+
+
+
+
+
