cf15eac1f37eaf37563b7d0d4ba2b74bc9a1b8f2
kartick sv <kartick sv>
2016-09-18 06:07:53 -0400

    Finish yaml

--- strings2.py
+++ strings2.py
@@ -1,31 +1,25 @@
 #Break ankle scene
-Al="Ah! Ouch!"
-You="Are you alright?"
-Al_1="I think I twisted my ankle. I really dont think I can go on."# al is on the ground 
+Break_ankle_prompt="Alex:Ah! Ouch!\nYou:Are you alright?\nAlex:I think I twisted my ankle. I really dont think I can go on."# al is on the ground 
 response_1="Common on, get up! We can't keep Eve waiting"
 response_2=" Ok let me help you up and we can take a look at it!"
 # Flash Light scene
 #while user takes a look at it
-Al_2="How bad is it? It's not too bad right?"
-Eve="Stop being a wuss! It's probably just a minor bruise."
-You_1="Well, I could tell if I had a flash light. I can barely see a thing!"
+Flashlight_prompt="Alex:How bad is it? It's not too bad right?\nEve:Stop being a wuss! It's probably just a minor bruise.\nYou:Well, I could tell if I had a flash light. I can barely see a thing!"
 
 #tractor beam silence even in the forest
-"......"
-Unid="Alex of Lafayette! You are the chosen one! You have been identified as the most environmentally concerned!"# both eve and alex are on the frame
-Eve_1="Hey look a UFO! Let me take a picture! User smile"
+
+Tracer_beam_prompt="Unidentified voice:Alex of Lafayette! You are the chosen one! You have been identified as the most environmentally concerned!\nEve:Hey look a UFO! Let me take a picture! smile!"
 response_5="What?"# takes picture requries surprise
 #If not smile
-Eve_2="User!!I said smile!"#else go into next mod
+If_not_smile_prompt="Eve:User!!I said smile!"#else go into next mod
 #alein interaction
-Eve_3="And Mr. unidentified voice what do you mean by that he is the most environmentally concerned? Did you know that I recycled 2 pop cans last week?"
+Alein_interaction_prompt="Eve:And Mr. unidentified voice what do you mean by that he is the most environmentally concerned? Did you know that I recycled 2 pop cans last week?\nYou:Talking about recycling, Did you know the average person has the opportunity to recycle more than 25,000 cans in a lifetime?""
 
-You_2="Talking about recycling, Did you know the average person has the opportunity to recycle more than 25,000 cans in a lifetime?"
+
+
+
 #Sounds of beams on alex he is getting sucked in
-Al_6="Oh God!What's happening?"
-Eve_4="Common user let's go!"
-You_7="But what about Alex? We can't abandon him"
-Eve_5="We dont have time! We gotta get going!"
+sucked_in_prompt="Alex:Oh God!What's happening?\nEve:Come on user let's go!\nYou:But what about Alex? We can't abandon him\nEve:We dont have time! We gotta get going!"
 response_3="Sorry Alex!I promise I'll come back!" #leads to node where you run into an alein
 response_4="No! I am not gonna leave him! We were all in this together!!" 
 
@@ -34,18 +28,17 @@ response_4="No! I am not gonna leave him! We were all in this together!!"
 
 
 #next screen blacks out , thud sounds 
-MUM="Honey! Wake up , you were talking in your sleep again!"
+MUM_prompt="Mom:Honey! Wake up , you were talking in your sleep again!"
 #wakes up in bed
-MUM_1= "You need start getting ready! Your bus will be here any time now!"
-You_5="For what?"
-MUM_2="It's the last day of school"
-You_6="Mom Did you know that..."
+wake_bed_prompt= "Mom:You need start getting ready! Your bus will be here any time now!\nYou:For what?\nMom:It's the last day of school"
+response_7="You:Mom Did you know that..."
 #at school
 promt="?:Hey you!"
 "........"
 
 
-
+#hell node 
+hell_prompt="Unfortunately that was a bad choice. Better luck next time."
 
 
 
