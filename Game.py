2426f5d8418d524c3ff05b095ae47cef49b0af93
Jane Du <zd53@cornell.edu>
2016-09-18 00:35:23 -0400

    Add some error catching for normal gui

--- Game.py
+++ Game.py
@@ -1,25 +1,32 @@
 from GameNode import GameNode as gn
 
 class Game:
-    start = gn("first thing", "./assets/images/a.gif")
-    second = gn("second thing", "./assets/images/b.gif", start, "tob", False)
-    # third = gn("also off the first thing", "./assets/images/c.gif", start, "toc")
-    end = gn("rome", "./assets/images/rome.gif")
+    end = gn("Thanks for playing our game!", "./assets/images/rome.gif")
+    start = gn("This should be the first panel.", "./assets/images/a.gif")
 
-    # fourth = gn("another", "./assets/images/a.gif")
-    # fifth = gn("asdf", "./assets/images/a.gif")
-    # sixth = gn("23f3f", "./assets/images/a.gif")
-    # seventh = gn("anasdfsdfother", "./assets/images/a.gif")
-    # eighth = gn("gwg23g", "./assets/images/a.gif")
-
-    # third.link("3-4", fourth)
-    # fourth.link("4-5", fifth)
-    # fifth.link("5-6", sixth)
-    # sixth.link("6-7", seventh)
-    # seventh.link("7-8", eighth)
-
+    second = gn("You are being asked to take a photo right now", "./assets/images/b.gif", start, "Go from statrt to second.", False)
     second.link( "But all roads lead to", end)
-    # eighth.link("But all roads lead to", end)
+    third = gn("The option without the photo", "./assets/images/c.gif", start, "Go from start to third. No photo.")
+     
+
+    fourth = gn("another", "./assets/images/a.gif")
+    fifth = gn("asdf", "./assets/images/a.gif")
+    sixth = gn("23f3f", "./assets/images/a.gif")
+    seventh = gn("anasdfsdfother", "./assets/images/a.gif")
+    eighth = gn("gwg23g", "./assets/images/a.gif")
+
+    third.link("3-4", fourth)
+    fourth.link("4-5", fifth)
+    fifth.link("5-6", sixth)
+    sixth.link("6-7", seventh)
+    seventh.link("7-8", eighth)
+
+
+    
+    
+    eighth.link("But all roads lead to", end)
+    dummy = gn("nothing here", "./assets/images/lost.gif", end, "Go to the deadone")
+
     # end.link ("But all roads lead to", end)
 
     def __init__(self):
--- gui.py
+++ gui.py
@@ -17,8 +17,16 @@ class MyFirstGUI:
         self.curr = game.start
         self.choices = self.curr.getOptions()
 
+        self.label = Label(master, text=self.curr.getText())
+        self.label.pack()
+
         self.B = tkinter.Button(master, text=self.choices[0], command=lambda: self.update(self.choices[0], self.camera))
         self.B.pack()
+        self.B2 = tkinter.Button(master, text="Hmm...", command=print("button 2"))
+        if (len(self.choices) > 1):
+            self.B2 = tkinter.Button(master, text=self.choices[1], command=lambda: self.update(self.choices[1], self.camera))
+
+        self.B2.pack()
 
         self.img = ImageTk.PhotoImage(self.curr.getImage())
         self.panel = Label(master, image=self.img)
@@ -30,19 +38,31 @@ class MyFirstGUI:
         """update the image and the buttons to the next node, waiting
         for the camera and happiness judgement to happen if necessary.
         """
+        if len(self.choices) == 0:
+           capture.stop_camera(camera)
+           self.master.destroy()
+        if self.label['text'] == "Thanks for playing our game!":
+           capture.stop_camera(camera)
+           self.master.destroy()
+
+        if (self.B2):
+            self.B2['text'] = "Hmm..."
+            self.B2['state'] = 'disabled'
 
         # string: fielname pass into capture
         # also pass it into expression test
 
-        photoname = "Pic " + str(self.picCount) + ".gif"
-        take_photo(camera, photoname)
-        self.picCount += 1
-
         # if canprogress == fasle, the node is expecting a picanalysis
         # runs picanalysis, creates new node. This is run again when getNext returns
         #    self.curr later on.
+        print("self.curr.canprogress is " + str(self.curr.canprogress))
         if (not(self.curr.canprogress)):
+            print("self.curr.canprogress is " + str(self.curr.canprogress))
+            photoname = "Pic" + str(self.picCount) + ".jpg"
+            capture.take_photo(camera, photoname)
+            self.picCount += 1       
             self.curr.canprogress = isHappyEnough(photoname)
+            print("self.curr.canprogress is then " + str(self.curr.canprogress))
 
         self.curr = self.curr.getNext(key)
         self.choices = self.curr.getOptions()
@@ -51,15 +71,17 @@ class MyFirstGUI:
         self.panel.configure(image=self.img)
         self.panel.image = self.img
 
+        self.label["text"] = self.curr.getText()
+
+
         self.B["text"] = key
         print(key)
         # nextnode = self.curr.getNext(key)
         # choices = list(self.curr.nexts.keys())
         # TODO: if cannot progress:
         #   capture.take_photo(camera)
-        #if len(choices) == 0:
-        #    capture.stop_camera(camera)
-        is_happy_enough = isHappyEnough(key)
+        
+        # is_happy_enough = isHappyEnough(key)
 
         # # self.curr = nextnode
         # print("something here")
